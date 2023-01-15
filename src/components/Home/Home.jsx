0389c2e19f7a96c2b4d3349fe44b24fc2aec3b5c
SPoluliakh <S_Poluliakh@ukr.net>
2023-01-15 22:40:20 +0200

    updated

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,8 +2,13 @@ import * as SC from './Home.styled';
 
 export const Home = () => {
   return (
-    <>
+    <SC.Wrap>
       <SC.HomeTitle>Save your contacts welcome page 💁‍♀️</SC.HomeTitle>
-    </>
+      <SC.Text>
+        Hello, this is your personal PhoneBook.It is designed to help your brain
+        not remember numbers.Create your own unique account and get the
+        opportunity to record and get your contacts from different divices.
+      </SC.Text>
+    </SC.Wrap>
   );
 };
--- src/components/Home/Home.styled.js
+++ src/components/Home/Home.styled.js
@@ -1,10 +1,26 @@
 import styled from 'styled-components';
 
 export const HomeTitle = styled.h1`
-  text-align: center;
   font-size: ${p => p.theme.fontSizes.ml}px;
   font-weight: ${p => p.theme.fontWeights.bold};
   @media (min-width: 768px) {
     font-size: ${p => p.theme.fontSizes.l}px;
+  }
+`;
+
+export const Wrap = styled.div`
+  margin: 0 auto;
+  width: 300px;
+  text-align: center;
+  @media (min-width: 768px) {
+    width: 420px;
+  }
+`;
+
+export const Text = styled.p`
+  font-size: ${p => p.theme.fontSizes.m}px;
+  font-weight: ${p => p.theme.fontWeights.medium};
+  @media (min-width: 768px) {
+    font-size: ${p => p.theme.fontSizes.ml}px;
   }
 `;
