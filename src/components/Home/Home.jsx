9c131e66fcf51dc79402dc7a079d2f28f4a429a1
SPoluliakh <S_Poluliakh@ukr.net>
2023-01-23 11:17:41 +0200

    fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,8 @@ export const Home = () => {
       <SC.HomeTitle>Save your contacts welcome page 💁‍♀️</SC.HomeTitle>
       <SC.Text>
         Hello, this is your personal PhoneBook.It is designed to help your brain
-        not remember numbers.Create your own unique account and get the
-        opportunity to record and get your contacts from different divices.
+        not to memorize the numbers.Create your own unique account and get the
+        opportunity to record and get your contacts from different devices.
       </SC.Text>
     </SC.Wrap>
   );
